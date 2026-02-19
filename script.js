88948123602858b66e3f4c93cb3682b89bb741cb
Simon <simonheripre4@gmail.com>
2026-02-19 16:36:41 +0100

    Add initial HTML structure, CSS styles, and JavaScript functionality for Etch-a-Sketch

---
+++ script.js
@@ -0,0 +1,35 @@
+const parentContainer = document.querySelector(".container");
+const generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", () => {
+  let widthParam = prompt("Width?");
+  let heigthPAram = prompt("Heigth?");
+  if (widthParam > 100) {
+    widthParam = 100;
+  }
+  if (heigthPAram > 100) {
+    heigthPAram = 100;
+  }
+
+  createGrid(widthParam, heigthPAram);
+});
+
+const createGrid = (pixLong, pixHaut) => {
+  parentContainer.innerHTML = ""
+  for (let col = 0; col < pixHaut; col++) {
+    const parentDiv = document.createElement("div");
+    parentDiv.classList.add("parentDiv");
+    parentContainer.appendChild(parentDiv);
+
+    for (let row = 0; row < pixLong; row++) {
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("newDiv");
+
+      parentDiv.appendChild(newDiv);
+
+      newDiv.addEventListener("mouseover", (element) => {
+        element.target.classList.add("activated");
+      });
+    }
+  }
+};
